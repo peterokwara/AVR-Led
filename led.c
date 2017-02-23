8cb0954f1bb26636955b76403e41a7737f4cd04c
P.Okwara <ojakaapeter@gmail.com>
2017-02-23 23:48:56 +0300

    Add comments to code

--- led.c
+++ led.c
@@ -2,10 +2,15 @@
 
 int main(void) {
   /* code */
+
+// Data direction register and assigning pin 0
+// on Port B to output
 DDRB = 0b00000001;
 
 while (1) {
   /* code */
+
+  // Assigning 5v to pin 0
   PORTB = 0b00000001;
   }
 return 0;
