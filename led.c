22474d40986dd823f85b81191829f51ce01bdde8
P.Okwara <ojakaapeter@gmail.com>
2017-02-24 15:44:30 +0300

    Use alternative way of switching pinb0 on and off

--- led.c
+++ led.c
@@ -5,16 +5,17 @@
 #include <util/delay.h>
 
 int main(void) {
-// OR operation, assign Port B to output in the
-// data direction register
-DDRB |= 0b00000001;
+// OR operation, shift 1 zero positions to the
+// left of the data direction register
+// to get the same result as before
+DDRB |= 1 << PINB0;
 
 while (1) {
   /* code */
 
-  PORTB = 0b00000001; // Assigning 5v to pin 0
+  PORTB |= 1 << PINB0; // Shift 1 zero to the left, or operation
   _delay_ms(800); // Delay of 800ms
-  PORTB = 0b00000000; // Assinging 0v to pin 0
+  PORTB &= ~(1 << PINB0); // Assinging 0v to pin 0
   _delay_ms(800); // Delay of 800ms
 
   }
