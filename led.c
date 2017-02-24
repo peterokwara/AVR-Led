f784ff6eb8f4204881e2d97bc028c8972f008e78
P.Okwara <ojakaapeter@gmail.com>
2017-02-24 14:50:08 +0300

    Make the led blink

--- led.c
+++ led.c
@@ -1,8 +1,10 @@
+
+#define F_CPU 1000000L // Define the speed of the microcontroller
+
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void) {
-  /* code */
-
 // Data direction register and assigning pin 0
 // on Port B to output
 DDRB = 0b00000001;
@@ -10,8 +12,11 @@ DDRB = 0b00000001;
 while (1) {
   /* code */
 
-  // Assigning 5v to pin 0
-  PORTB = 0b00000001;
+  PORTB = 0b00000001; // Assigning 5v to pin 0
+  _delay_ms(800); // Delay of 800ms
+  PORTB = 0b00000000; // Assinging 0v to pin 0
+  _delay_ms(800); // Delay of 800ms
+
   }
 return 0;
 }
